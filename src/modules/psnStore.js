0ad938875acddefed85a69891e215100cd4e8e95
Joni Kanerva <jhonny@jhonny.org>
2018-06-23 09:25:55 +0300

    import only required, coz maybe a smaller bundle

--- src/modules/psnStore.js
+++ src/modules/psnStore.js
@@ -1,21 +1,22 @@
-import * as R from 'ramda'
+import { compose, filter, head, map, pathOr, propEq } from 'ramda'
 import axios from 'axios'
 
-const parseGames = R.compose(
-  R.map(game => ({
-    name: R.prop('name', game),
-    date: R.prop('release-date', game),
-    url: R.prop('thumbnail-url-base', game),
-    id: R.prop('id', game),
-    price: R.path(
-      ['prices', 'plus-user', 'actual-price', 'display'],
-      R.head(R.propOr([], 'skus', game))
-    )
-  })),
-  R.map(R.prop('attributes')),
-  R.map(data => R.assocPath(['attributes', 'id'], R.prop('id', data), data)),
-  R.filter(R.propEq('type', 'game')),
-  R.pathOr([], ['data', 'included'])
+const createGameObject = game => ({
+  name: pathOr('', ['attributes', 'name'], game),
+  date: pathOr('', ['attributes', 'release-date'], game),
+  url: pathOr('', ['attributes', 'thumbnail-url-base'], game),
+  id: pathOr('', ['id'], game),
+  price: pathOr(
+    '',
+    ['prices', 'plus-user', 'actual-price', 'display'],
+    head(pathOr([], ['attributes', 'skus'], game))
+  )
+})
+
+const parseGames = compose(
+  map(createGameObject),
+  filter(propEq('type', 'game')),
+  pathOr([], ['data', 'included'])
 )
 
 export const fetchGames = url => axios.get(url).then(parseGames)
