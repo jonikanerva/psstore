e465c4bf263b70af7c152d51c46c8f16e976b6ad
Joni Kanerva <jhonny@jhonny.org>
2018-07-01 10:20:33 +0300

    discounted before upcoming

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -9,31 +9,31 @@ const upcomingGamesUrl =
   'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-COMINGSOON?sort=release_date&direction=desc&size=100&bucket=games&start=0'
 
 const discountGamesUrl =
-  'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-PRICEDROPSCHI?sort=release_date&direction=desc&platform=ps4&game_content_type=games&size=200&bucket=games&start=0'
+  'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-PRICEDROPSCHI?sort=release_date&direction=desc&platform=ps4&game_content_type=games&size=80&bucket=games&start=0'
 
 class App extends Component {
   render() {
     return (
       <div className="parent">
         <div className="list">
-          <a name="new" href="#upcoming">
+          <a name="new" href="#discounted">
             <div className="header">New</div>
           </a>
           <Games url={newGamesUrl} sort="desc" />
         </div>
 
         <div className="list">
-          <a name="upcoming" href="#discounted">
-            <div className="header">Upcoming</div>
-          </a>
-          <Games url={upcomingGamesUrl} sort="asc" />
-        </div>
-
-        <div className="list">
-          <a name="discounted" href="#new">
+          <a name="discounted" href="#upcoming">
             <div className="header">Discounted</div>
           </a>
           <Games url={discountGamesUrl} sort="discount" />
+        </div>
+
+        <div className="list">
+          <a name="upcoming" href="#new">
+            <div className="header">Upcoming</div>
+          </a>
+          <Games url={upcomingGamesUrl} sort="asc" />
         </div>
       </div>
     )
