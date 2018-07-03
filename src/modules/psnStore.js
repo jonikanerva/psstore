f5538302b6265662498579d71d2ddd1e9cf0c4c6
Joni Kanerva <jhonny@jhonny.org>
2018-07-03 08:33:48 +0300

    fetch screenshots

--- src/modules/psnStore.js
+++ src/modules/psnStore.js
@@ -1,27 +1,35 @@
-import { compose, filter, head, map, pathOr, propEq } from 'ramda'
+import * as R from 'ramda'
 import axios from 'axios'
 
 const createGameObject = game => {
-  const skus = head(pathOr([], ['attributes', 'skus'], game))
+  const skus = R.head(R.pathOr([], ['attributes', 'skus'], game))
 
   return {
-    name: pathOr('', ['attributes', 'name'], game),
-    date: pathOr('', ['attributes', 'release-date'], game),
-    url: pathOr('', ['attributes', 'thumbnail-url-base'], game),
-    id: pathOr('', ['id'], game),
-    price: pathOr('', ['prices', 'plus-user', 'actual-price', 'display'], skus),
-    discountDate: pathOr(
+    name: R.pathOr('', ['attributes', 'name'], game),
+    date: R.pathOr('', ['attributes', 'release-date'], game),
+    url: R.pathOr('', ['attributes', 'thumbnail-url-base'], game),
+    id: R.pathOr('', ['id'], game),
+    price: R.pathOr(
+      '',
+      ['prices', 'plus-user', 'actual-price', 'display'],
+      skus
+    ),
+    discountDate: R.pathOr(
       '1975-01-01T00:00:00Z',
       ['prices', 'plus-user', 'availability', 'start-date'],
       skus
+    ),
+    screenShots: R.map(
+      R.prop('url'),
+      R.pathOr([], ['attributes', 'media-list', 'screenshots'], game)
     )
   }
 }
 
-const parseGames = compose(
-  map(createGameObject),
-  filter(propEq('type', 'game')),
-  pathOr([], ['data', 'included'])
+const parseGames = R.compose(
+  R.map(createGameObject),
+  R.filter(R.propEq('type', 'game')),
+  R.pathOr([], ['data', 'included'])
 )
 
 export const fetchGames = url => axios.get(url).then(parseGames)
