bf11f0493fe8ade5acc8a46a3a7a2af0a897a09e
Joni Kanerva <jhonny@jhonny.org>
2018-07-03 08:43:19 +0300

    use fetch

--- src/modules/psnStore.js
+++ src/modules/psnStore.js
@@ -1,14 +1,14 @@
 import * as R from 'ramda'
-import axios from 'axios'
 
 const createGameObject = game => {
   const skus = R.head(R.pathOr([], ['attributes', 'skus'], game))
+  const images = R.pathOr([], ['attributes', 'media-list', 'screenshots'], game)
 
   return {
     name: R.pathOr('', ['attributes', 'name'], game),
     date: R.pathOr('', ['attributes', 'release-date'], game),
     url: R.pathOr('', ['attributes', 'thumbnail-url-base'], game),
-    id: R.pathOr('', ['id'], game),
+    id: R.propOr('', 'id', game),
     price: R.pathOr(
       '',
       ['prices', 'plus-user', 'actual-price', 'display'],
@@ -19,17 +19,17 @@ const createGameObject = game => {
       ['prices', 'plus-user', 'availability', 'start-date'],
       skus
     ),
-    screenShots: R.map(
-      R.prop('url'),
-      R.pathOr([], ['attributes', 'media-list', 'screenshots'], game)
-    )
+    screenShots: R.map(R.prop('url'), images)
   }
 }
 
 const parseGames = R.compose(
   R.map(createGameObject),
   R.filter(R.propEq('type', 'game')),
-  R.pathOr([], ['data', 'included'])
+  R.propOr([], 'included')
 )
 
-export const fetchGames = url => axios.get(url).then(parseGames)
+export const fetchGames = url =>
+  fetch(url)
+    .then(res => res.json())
+    .then(parseGames)
