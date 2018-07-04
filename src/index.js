ab20484e5acab854933c8aec57f3105d2f4cc12b
Joni Kanerva <jhonny@jhonny.org>
2018-07-04 22:05:19 +0300

    oops

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app/App'
-import { unregister } from './registerServiceWorker'
+import { unregister } from './modules/registerServiceWorker'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
