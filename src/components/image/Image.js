f4fb2793bf5f2392a5e57e913411ba3dba030e30
Joni Kanerva <jhonny@jhonny.org>
2018-07-09 22:22:52 +0300

    make image component pure

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,65 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
 import TrackVisibility from 'react-on-screen'
-import Screenshots from '../screenshots/Screenshots'
 import './Image.css'
 
 const ImageSrc = props => (
   <img src={props.isVisible ? props.url : ''} alt={props.name} />
 )
 
-class Image extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      showScreenshots: false
-    }
-  }
-
-  toggleScreenshots = e => {
-    this.setState({
-      showScreenshots: !this.state.showScreenshots
-    })
-
-    // prevent scrolling on body when the screenshots are visible
-    document.body.style.overflow = this.state.showScreenshots
-      ? 'visible'
-      : 'hidden'
-
-    // don't follow the link on click
-    e.preventDefault()
-  }
-
-  render() {
-    const { showScreenshots } = this.state
-    const {
-      description,
-      genres,
-      name,
-      screenshots,
-      studio,
-      url,
-      videos
-    } = this.props
-
-    return (
-      <div onClick={this.toggleScreenshots}>
-        <TrackVisibility once={true} offset={200} className="image">
-          <ImageSrc url={url} name={name} />
-        </TrackVisibility>
-        <Screenshots
-          description={description}
-          genres={genres}
-          name={name}
-          screenshots={screenshots}
-          show={showScreenshots}
-          studio={studio}
-          videos={videos}
-          onClose={this.toggleScreenshots}
-        />
-      </div>
-    )
-  }
-}
+const Image = props => (
+  <TrackVisibility once={true} offset={200} className="image">
+    <ImageSrc url={props.url} name={props.name} />
+  </TrackVisibility>
+)
 
 export default Image
