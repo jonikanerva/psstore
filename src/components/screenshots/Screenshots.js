e76134e35cb9ef6465503f503196e6e5a6c1e65f
Joni Kanerva <jhonny@jhonny.org>
2018-07-09 20:02:51 +0300

    add more info

--- src/components/screenshots/Screenshots.js
+++ src/components/screenshots/Screenshots.js
@@ -2,24 +2,42 @@ import React, { Component } from 'react'
 import './Screenshots.css'
 
 class Screenshots extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      name: props.name,
-      onClose: props.onClose,
-      screenshots: props.screenshots
-    }
-  }
-
   render() {
-    const { name, screenshots } = this.state
+    const {
+      description,
+      genres,
+      name,
+      screenshots,
+      studio,
+      videos
+    } = this.props
 
     return !this.props.show ? null : (
       <div className="screenshots-modal">
+        <div className="screenshots-name">{name}</div>
+        <div className="screenshots-byline">
+          {genres} by {studio}
+        </div>
+
         {screenshots.map((screenshot, i) => (
           <img src={screenshot} alt={name} key={i} />
         ))}
+
+        {videos.map((video, i) => (
+          <video
+            preload="metadata"
+            controls
+            muted
+            width="100%"
+            src={video}
+            key={i}
+          />
+        ))}
+
+        <div
+          className="screenshots-description"
+          dangerouslySetInnerHTML={{ __html: description }}
+        />
       </div>
     )
   }
