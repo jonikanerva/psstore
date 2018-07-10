47bfd22777f5c46fc864ed511db907314530771e
Joni Kanerva <jhonny@jhonny.org>
2018-07-10 08:43:07 +0300

    extract everything

--- src/components/screenshots/Screenshots.js
+++ src/components/screenshots/Screenshots.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Screenshots.css'
 
+const Name = props => <div className="screenshots-name">{props.name}</div>
+
+const ByLine = props => (
+  <div className="screenshots-byline">
+    {props.genres} by {props.studio}
+  </div>
+)
+
 const Images = props =>
   props.screenshots.map((screenshot, i) => (
     <img src={screenshot} alt={props.name} key={i} />
@@ -11,24 +19,24 @@ const Videos = props =>
     <video preload="metadata" controls muted width="100%" src={video} key={i} />
   ))
 
+const Description = props => (
+  <div
+    className="screenshots-description"
+    dangerouslySetInnerHTML={{ __html: props.description }}
+  />
+)
+
 const Screenshots = props => {
   const { description, genres, name, screenshots, studio, videos } = props.game
   const { show, onClose } = props
 
   return !show ? null : (
     <div className="screenshots-modal" onClick={onClose}>
-      <div className="screenshots-name">{name}</div>
-      <div className="screenshots-byline">
-        {genres} by {studio}
-      </div>
-
+      <Name name={name} />
+      <ByLine genres={genres} studio={studio} />
       <Images screenshots={screenshots} name={name} />
       <Videos videos={videos} />
-
-      <div
-        className="screenshots-description"
-        dangerouslySetInnerHTML={{ __html: description }}
-      />
+      <Description description={description} />
     </div>
   )
 }
