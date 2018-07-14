ee2c8b31087f76c54516c474057b1ca35fac8b75
Joni Kanerva <jhonny@jhonny.org>
2018-07-14 17:03:13 +0300

    simplify games

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import Game from '../game/Game'
-import Error from '../error/Error'
-import Loading from '../spinner/Spinner'
-import fetchGames from '../../modules/psnStore'
-import localStorage from '../../modules/localStorage'
 import './Games.css'
 
 const GameRows = props =>
@@ -11,41 +7,11 @@ const GameRows = props =>
     <Game game={game} label={props.label} key={game.id} />
   ))
 
-class Games extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      error: false,
-      loading: true,
-      games: localStorage.getItem(props.label)
-    }
-  }
-
-  componentDidMount() {
-    const { url, label } = this.props
-
-    fetchGames(url, label)
-      .then(games => {
-        this.setState({ games, loading: false })
-        localStorage.setItem(label, games)
-      })
-      .catch(() => this.setState({ error: true }))
-  }
-
-  render() {
-    const { games, loading, error } = this.state
-    const { label } = this.props
-    const showGames = !!games && !error
-
-    return (
-      <div className="games-table">
-        <Error error={error} />
-        <Loading loading={loading} />
-        {showGames && <GameRows games={games} label={label} />}
-      </div>
-    )
-  }
-}
+const Games = ({ label, games }) =>
+  !games ? null : (
+    <div className="games-table">
+      <GameRows games={games} label={label} />
+    </div>
+  )
 
 export default Games
