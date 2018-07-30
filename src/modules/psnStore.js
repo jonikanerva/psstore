ce851bf38569f80945089768176a8922e0d16789
Joni Kanerva <jhonny@jhonny.org>
2018-07-30 19:51:01 +0300

    fetch all discounted games

--- src/modules/psnStore.js
+++ src/modules/psnStore.js
@@ -1,13 +1,13 @@
 import * as R from 'ramda'
 
-export const newGamesUrl =
+const newGamesUrl =
   'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-GAMENEWTHISMONTH?sort=release_date&direction=desc&platform=ps4&game_content_type=games%2Cbundles&size=100&bucket=games&start=0'
 
-export const upcomingGamesUrl =
+const upcomingGamesUrl =
   'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-COMINGSOON?sort=release_date&direction=desc&size=100&bucket=games&start=0'
 
-export const discountGamesUrl =
-  'https://store.playstation.com/valkyrie-api/en/FI/19/container/STORE-MSF75508-PRICEDROPSCHI?sort=release_date&direction=desc&platform=ps4&game_content_type=games%2Cbundles&size=80&bucket=games&start=0'
+const baseNavUrl =
+  'https://store.playstation.com/valkyrie-api/en/FI/999/storefront/STORE-MSF75508-BASE'
 
 const createGameObject = game => {
   const skus = R.head(R.pathOr([], ['attributes', 'skus'], game))
@@ -51,13 +51,27 @@ const sortGames = sort => {
   }
 }
 
+const parseDiscountUrls = R.compose(
+  R.map(
+    id =>
+      `https://store.playstation.com/valkyrie-api/en/FI/19/container/${id}?size=200&start=0&platform=ps4&game_content_type=games%2Cbundles&bucket=games`
+  ),
+  R.map(R.prop('target-container-id')),
+  R.pathOr(
+    [{ 'target-container-id': 'STORE-MSF75508-PRICEDROPSCHI' }],
+    [0, 'items']
+  ),
+  R.filter(R.propEq('name', 'Deals & Offers')),
+  R.pathOr([], ['data', 'attributes', 'navigation', 0, 'submenu'])
+)
+
 const parseGames = R.compose(
   R.map(createGameObject),
   R.filter(R.propEq('type', 'game')),
   R.propOr([], 'included')
 )
 
-const fetchUrl = (url, label) =>
+const fetchUrl = url =>
   fetch(url)
     .then(res => res.json())
     .then(parseGames)
@@ -68,7 +82,14 @@ const fetchUpcomingGames = () =>
   fetchUrl(upcomingGamesUrl).then(sortGames('asc'))
 
 const fetchDiscountedGames = () =>
-  fetchUrl(discountGamesUrl).then(sortGames('discounted'))
+  fetch(baseNavUrl)
+    .then(res => res.json())
+    .then(parseDiscountUrls)
+    .then(R.map(url => fetchUrl(url)))
+    .then(fetches => Promise.all(fetches))
+    .then(R.mergeAll)
+    .then(R.values)
+    .then(sortGames('discounted'))
 
 const fetchGames = () =>
   Promise.all([
