d957fb1604ddbbf202263fe90c7f488151d341cc
Joni Kanerva <jhonny@jhonny.org>
2018-08-31 18:19:00 +0300

    add optional message

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Error.css'
 
-const Error = () => <div className="error">an error occured ¯\_(ツ)_/¯</div>
+const Error = props => (
+  <div className="error">{props.message || 'an error occured ¯\\_(ツ)_/¯'}</div>
+)
 
 export default Error
