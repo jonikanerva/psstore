2f9af980845eed451c7390e369b23eaea8306243
Joni Kanerva <jhonny@jhonny.org>
2018-08-31 17:18:33 +0300

    move state from app to games and connect games to redux store

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import * as R from 'ramda'
-import fetchGames from '../../modules/psnStore'
-import localStorage from '../../modules/localStorage'
 import GamesList from '../gameslist/GamesList'
 import Screenshots from '../screenshots/Screenshots'
 import Error from '../error/Error'
@@ -12,32 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      error: false,
-      loading: true,
-      newGames: localStorage.getItem('newGames'),
-      discountedGames: localStorage.getItem('discountedGames'),
-      upcomingGames: localStorage.getItem('upcomingGames')
-    }
-
     this.renderScreenshots = this.renderScreenshots.bind(this)
-    this.renderGameList = this.renderGameList.bind(this)
-  }
-
-  componentDidMount() {
-    fetchGames()
-      .then(({ newGames, discountedGames, upcomingGames }) => {
-        this.setState({
-          loading: false,
-          newGames,
-          discountedGames,
-          upcomingGames
-        })
-        localStorage.setItem('newGames', newGames)
-        localStorage.setItem('discountedGames', discountedGames)
-        localStorage.setItem('upcomingGames', upcomingGames)
-      })
-      .catch(() => this.setState({ error: true }))
   }
 
   renderScreenshots({ match }) {
@@ -57,28 +30,11 @@ class App extends Component {
     return game ? <Screenshots game={game} /> : <Error />
   }
 
-  renderGameList() {
-    const { newGames, upcomingGames, discountedGames, loading } = this.state
-
-    return (
-      <GamesList
-        loading={loading}
-        newGames={newGames}
-        upcomingGames={upcomingGames}
-        discountedGames={discountedGames}
-      />
-    )
-  }
-
   render() {
-    if (this.state.error) {
-      return <Error />
-    }
-
     return (
       <Router>
         <React.Fragment>
-          <Route exact={true} path="/" render={this.renderGameList} />
+          <Route exact={true} path="/" component={GamesList} />
           <Route path="/g/:gameId" render={this.renderScreenshots} />
         </React.Fragment>
       </Router>
