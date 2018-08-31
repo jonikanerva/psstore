0119968c6985f3ee845fcba1ed6f1ef68ef1e53e
Joni Kanerva <jhonny@jhonny.org>
2018-08-31 11:21:07 +0300

    move header into games

--- src/components/games/Games.js
+++ src/components/games/Games.js
@@ -1,14 +1,30 @@
 import React from 'react'
 import Game from '../game/Game'
+import Loading from '../spinner/Spinner'
 import './Games.css'
+
+const Header = ({ label, linkto, loading }) => (
+  <a name={label} href={`#${linkto}`}>
+    <div className="games-header">
+      <div>{label}</div>
+      <div className="games-header-loading">
+        <Loading loading={loading} />
+      </div>
+    </div>
+  </a>
+)
 
 const GameRows = ({ games, label }) =>
   games.map(game => <Game game={game} label={label} key={game.id} />)
 
-const Games = ({ label, games }) =>
+const Games = ({ label, linkto, loading, games }) =>
   !games ? null : (
-    <div className="games-table">
-      <GameRows games={games} label={label} />
+    <div className="games-list">
+      <Header label={label} linkto={linkto} loading={loading} />
+
+      <div className="games-table">
+        <GameRows games={games} label={label} />
+      </div>
     </div>
   )
 
--- src/components/gameslist/GamesList.js
+++ src/components/gameslist/GamesList.js
@@ -1,38 +1,27 @@
 import React from 'react'
 import Games from '../games/Games'
-import Loading from '../spinner/Spinner'
 import ScrollToTopOnMount from '../scroll/ScrollToTopOnMount'
-
 import './GamesList.css'
 
-const Header = ({ label, linkto, loading }) => (
-  <a name={label} href={`#${linkto}`}>
-    <div className="gameslist-header">
-      <div>{label}</div>
-      <div className="gameslist-header-loading">
-        <Loading loading={loading} />
-      </div>
-    </div>
-  </a>
-)
-
 const GamesLists = ({ newGames, upcomingGames, discountedGames, loading }) => (
-  <div className="gameslist-parent">
+  <div className="gameslist-container">
     <ScrollToTopOnMount />
-    <div className="gameslist-list">
-      <Header label="new" linkto="discounted" loading={loading} />
-      <Games label="new" games={newGames} />
-    </div>
 
-    <div className="gameslist-list">
-      <Header label="discounted" linkto="upcoming" loading={loading} />
-      <Games label="discounted" games={discountedGames} />
-    </div>
+    <Games label="new" linkto="discounted" loading={loading} games={newGames} />
 
-    <div className="gameslist-list">
-      <Header label="upcoming" linkto="new" loading={loading} />
-      <Games label="upcoming" games={upcomingGames} />
-    </div>
+    <Games
+      label="discounted"
+      linkto="upcoming"
+      loading={loading}
+      games={discountedGames}
+    />
+
+    <Games
+      label="upcoming"
+      linkto="new"
+      loading={loading}
+      games={upcomingGames}
+    />
   </div>
 )
 
