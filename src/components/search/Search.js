b83d19f853277226853f609dd1983fb2748ba7df
Joni Kanerva <jhonny@jhonny.org>
2018-08-31 18:22:38 +0300

    pass linkto

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -3,7 +3,11 @@ import Games from '../games/Games'
 import { searchGames } from '../../modules/psnStore'
 
 const Search = props => (
-  <Games label="search" fetch={() => searchGames(props.match.params.search)} />
+  <Games
+    label="search"
+    linkto=""
+    fetch={() => searchGames(props.match.params.search)}
+  />
 )
 
 export default Search
