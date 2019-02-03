994f784c0bb9862d42e449882b4422b0f0891b50
Joni Kanerva <jhonny@jhonny.org>
2019-02-03 22:16:23 +0200

    update search field

--- src/components/searchfield/SearchField.js
+++ src/components/searchfield/SearchField.js
@@ -3,13 +3,13 @@ import './SearchField.css'
 
 const SearchField = ({ label, linkto }) => (
   <div className="searchfield">
+    <a name={label} href={`#${linkto}`} className="searchfield__link">
+      <div className="games--header">{label}</div>
+    </a>
     <form method="GET" action="/search">
       <input type="text" name="q" className="searchfield__text" />
       <input type="submit" value="Search" className="searchfield__submit" />
     </form>
-    <a name={label} href={`#${linkto}`} className="searchfield__link">
-      top
-    </a>
   </div>
 )
 
