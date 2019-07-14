b1803e12b3343561aa5cf70e7dc3ca0b6446139a
Joni Kanerva <jhonny@jhonny.org>
2019-07-14 09:22:58 +0300

    fix search

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './SearchField.css'
 
-const SearchField = () => (
-  <div className="searchfield">
-    <div className="searchfield--form">
-      <form method="GET" action="/search">
-        <input type="text" name="q" className="searchfield--text" />
-        <input type="submit" value="Search" className="searchfield--submit" />
-      </form>
-    </div>
-  </div>
-)
+const SearchField = ({ searchString }) => {
+  const [value, setValue] = useState()
 
+  useEffect(() => setValue(searchString), [])
+
+  return (
+    <div className="searchfield">
+      <div className="searchfield--form">
+        <form method="GET" action="/search">
+          <input
+            type="text"
+            value={value}
+            name="q"
+            onChange={e => setValue(e.target.value)}
+            className="searchfield--text"
+          />
+          <input type="submit" value="Search" className="searchfield--submit" />
+        </form>
+      </div>
+    </div>
+  )
+}
 export default SearchField
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,19 +3,28 @@ import queryString from 'query-string'
 import * as R from 'ramda'
 import Games from './Games'
 import SearchField from './SearchField'
+import ScrollToTopOnMount from './ScrollToTopOnMount'
+import Navigation from './Navigation'
 import { searchGames } from '../modules/psnStore'
 
-const parseQ = props => R.prop('q', queryString.parse(props.location.search))
+const SearchResults = props => {
+  const searchString = R.prop('q', queryString.parse(props.location.search))
 
-const SearchResults = props => (
-  <div className="searchresults">
-    <Games
-      label="Found"
-      linkto="field"
-      fetch={() => searchGames(parseQ(props))}
-    />
-    <SearchField label="Search" linkto="search" />
-  </div>
-)
+  return (
+    <React.Fragment>
+      <ScrollToTopOnMount />
+      <Navigation />
+      <SearchField label="search" linkto="search" searchString={searchString} />
+
+      {searchString && (
+        <Games
+          label="search"
+          linkto="field"
+          fetch={() => searchGames(searchString)}
+        />
+      )}
+    </React.Fragment>
+  )
+}
 
 export default SearchResults
