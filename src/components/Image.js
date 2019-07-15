bc5ab021079159663d818de18b92d1090b0d9dc3
Joni Kanerva <jhonny@jhonny.org>
2019-07-15 14:49:52 +0300

    use react-visibility-observer

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,21 @@
 import React from 'react'
-import TrackVisibility from 'react-on-screen'
+import VisibilityObserver, {
+  useVisibilityObserver
+} from 'react-visibility-observer'
 import './Image.css'
 
-const ImageSrc = ({ isVisible, url, name }) => (
-  <img src={isVisible ? url : ''} alt={name} title={name} className="image" />
-)
+const ImageSrc = ({ url, name }) => {
+  const { isVisible } = useVisibilityObserver()
 
-const Image = ({ url, name }) => (
-  <TrackVisibility once={true} offset={1000}>
-    <ImageSrc url={url} name={name} />
-  </TrackVisibility>
-)
-
+  return (
+    <img src={isVisible ? url : ''} alt={name} title={name} className="image" />
+  )
+}
+const Image = ({ url, name }) => {
+  return (
+    <VisibilityObserver>
+      <ImageSrc url={url} name={name} />
+    </VisibilityObserver>
+  )
+}
 export default Image
