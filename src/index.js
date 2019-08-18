9a12a2975ef3e6064a0989b209c9422ce7eb3650
Joni Kanerva <jhonny@jhonny.org>
2019-08-18 09:19:27 +0300

    remove redux and reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
-import { Provider } from 'react-redux'
 import App from './components/App'
-import gamesReducer from './reducers/games'
 
-const reducer = combineReducers({ games: gamesReducer })
-const store = createStore(reducer)
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
